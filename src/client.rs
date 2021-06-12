415cf03339544f8510502718c8637825090054a0
dani <danielorihuelarodriguez@gmail.com>
2021-06-12 17:47:09 +0200

    feat: client module

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,36 @@
+use crate::messages::{decode_message, encode_message, MessageType};
 
+use std::error::Error;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+pub fn ask_exit_node_for_proof(mut stream: TcpStream) -> Result<String, Box<dyn Error>> {
+    let message = encode_message(&MessageType::NeedProof, "");
+    stream.write_all(message.as_bytes()).unwrap();
+
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+
+    let (_, data) = decode_message(&response);
+
+    Ok(String::from(data))
+}
+
+pub fn ask_directory_authority_to_verify_proof(
+    mut stream: TcpStream,
+    proof: &str,
+) -> Result<bool, Box<dyn Error>> {
+    let message = encode_message(&MessageType::VerifyProof, proof);
+    stream.write_all(message.as_bytes()).unwrap();
+
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+
+    let (message_type, _) = decode_message(&response);
+
+    match message_type {
+        Some(MessageType::VerifiedProof) => return Ok(true),
+        Some(MessageType::IncorrectProof) => return Ok(false),
+        Some(_) | None => return Ok(false),
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,8 @@ pub use server::handle_client;
 pub use server::init_directory_authority;
 pub use server::init_exit_node;
 
+pub mod client;
+pub use client::ask_directory_authority_to_verify_proof;
+pub use client::ask_exit_node_for_proof;
 
 mod zpie;
